ed9febf5442b1e2f4dfa3a169110abd96a93fde7
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2025-02-02 16:22:09 -0800

    added registration logic

--- app/screens/AuthenticationNavigationStack/RegistrationScreen.tsx
+++ app/screens/AuthenticationNavigationStack/RegistrationScreen.tsx
@@ -1,16 +1,37 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TextInput } from 'react-native'
 import React from 'react'
 import { useNavigation } from 'expo-router'
+import styles from '../styles/RegistrationScreenStyle'
+import { useLazySignUpQuery } from '../../slices/tAuthApiSlice';
+import { useState } from 'react';
+
 
 export default function RegistrationScreen() {
   const navigation = useNavigation()
+  const [triggerSignUp, { data, isLoading, isFetching, error }] = useLazySignUpQuery();
+  const [userParams, setUserParams] = useState<{ email: string; password: string }>({
+    email: "",
+    password: ""
+  })
 
   const handleNavigation = () => {
     navigation.navigate("Login")
   }
+
+  const handleSubmit = () => {
+    triggerSignUp(userParams)
+  }
+
+  const handleTextChange = (key: keyof typeof userParams, input: string) => {
+    setUserParams((prev) => ({ ...prev, [key]: input }))
+  }
+
   return (
     <View>
-      <Button title="To Login" onPress={() => { handleNavigation() }}/>
+      <TextInput value={userParams.email} onChangeText={(text) => { handleTextChange("email", text) }} style={styles.input} placeholder='Email'/>
+      <TextInput value={userParams.password} onChangeText={(text) => { handleTextChange("password", text) }} style={styles.input} placeholder='Password'/>
+      <Button title="Create Account" onPress={() => { handleSubmit() }} />
+      <Button title="Already have an account? Login" onPress={() => { handleNavigation() }} />
     </View>
   )
 }
