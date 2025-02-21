abf7f2436651e66b6956196f23c58a674518808a
bchh325 <bbchhorb@gmail.com>
2025-02-20 19:43:26 -0800

    update Picture.tsx uri

--- app/components/Picture.tsx
+++ app/components/Picture.tsx
@@ -10,7 +10,7 @@ export default function Picture({ item }: Props) {
     console.debug("string item ")
     return (
         <View style={styles.container}>
-            <Image alt="Testing alt image" source={{uri: item.toString()}} style={styles.image} />
+            <Image source={{uri: item.toString()}} style={styles.image} />
         </View>
     )
 }
