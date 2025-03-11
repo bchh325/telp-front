311538bbbc4bba8b13fe501d2e5ed9e797e189d1
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2025-03-10 18:26:35 -0700

    button placement

--- app/components/ActionBar.tsx
+++ app/components/ActionBar.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import classNames from '../utils/classnames'
+import IconButton from './IconButton'
 
 export default function ActionBar() {
+
+    const containerClasses = classNames([
+        true && styles.container
+    ])
+
+    const actionClasses = classNames([
+        true && styles.actions
+    ])
+
     return (
-        <View style={{ flex: 1, width: "100%", borderColor: "yellow", borderWidth: 5 }}>
-            <Text style={{ color: "green" }}>ASDFSAFASDSA</Text>
+        <View style={containerClasses}>
+            <View style={actionClasses}>
+                <IconButton />
+                <IconButton />
+                <IconButton />
+                <IconButton />
+            </View>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flex: 1, 
+        width: "100%",
+        borderColor: "yellow", 
+        borderWidth: 1,
+        flexDirection: "row",
+
+    },
+    actions: {
+        borderColor: "red",
+        borderWidth: 2,
+        marginLeft: "auto",
+        display: "flex",
+        rowGap: 40,
+        marginBottom: "auto"
+    }
+})
--- app/components/IconButton.tsx
+++ app/components/IconButton.tsx
@@ -4,13 +4,16 @@ import React from 'react'
 export default function IconButton() {
   return (
     <View style={styles.iconButtonContainer}>
-      <Text>IconButton</Text>
+      <Text style={{color: "white"}}>IconButton</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     iconButtonContainer: {
-
+      borderColor: "yellow",
+      borderWidth: 1,
+      aspectRatio: 1,
+      width: 50
     }
 })
