86e1e3795f327afe53cc5a8a714e441580ba6900
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2025-03-10 18:30:51 -0700

    add info panel

--- app/components/ActionBar.tsx
+++ app/components/ActionBar.tsx
@@ -13,8 +13,26 @@ export default function ActionBar() {
         true && styles.actions
     ])
 
+    const infoClasses = classNames([
+        true && styles.info
+    ])
+
     return (
         <View style={containerClasses}>
+            <View style={infoClasses}>
+                <Text>
+                    info panel
+                </Text>
+                <Text>
+                    info panel
+                </Text>
+                <Text>
+                    info panel
+                </Text>
+                <Text>
+                    info panel
+                </Text>
+            </View>
             <View style={actionClasses}>
                 <IconButton />
                 <IconButton />
@@ -34,14 +52,18 @@ const styles = StyleSheet.create({
         borderColor: "yellow", 
         borderWidth: 1,
         flexDirection: "row",
-
+        paddingBottom: 50
     },
     actions: {
         borderColor: "red",
         borderWidth: 2,
         marginLeft: "auto",
         display: "flex",
-        rowGap: 40,
-        marginBottom: "auto"
+        justifyContent: "space-evenly"
+    },
+    info: {
+        borderColor: "red",
+        borderWidth: 1,
+        flex: 1
     }
 })
